f06c9279ef68ccd182dafc2d261267257d4bad7a
Priyansh Saini <103327623+Priyansh4615@users.noreply.github.com>
2022-04-09 17:43:32 +0000

    imported BeautifulSoup

--- scraper.py
+++ scraper.py
@@ -1,1 +1,16 @@
-print('hello world')
+import requests
+
+from bs4 import BeautifulSoup
+YOUTUBE_TRENDING_URL = 'https://www.youtube.com/feed/trending'
+
+response = requests.get(YOUTUBE_TRENDING_URL)
+
+print('Status Code',response.status_code)
+# print('Output', response.text[:1000])
+
+with open('trending.html', 'w') as f:
+  f.write(response.text)
+
+doc = BeautifulSoup('response.text', 'html.parser')
+
+print('Page title:', doc.title)
